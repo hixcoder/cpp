dbc1ab12e360d2ef4a4f67143c342a0dee5d7ea6
Hamza Boumahdi <hboumahd@e1r5p2.1337.ma>
2022-12-09 12:27:01 +0100

    working on the ex04 module01

--- module 01/ex04/MyHeader.hpp
+++ module 01/ex04/MyHeader.hpp
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*   MyHeader.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/08 15:43:50 by hboumahd          #+#    #+#             */
-/*   Updated: 2022/12/09 12:25:35 by hboumahd         ###   ########.fr       */
+/*   Created: 2022/12/08 15:51:48 by hboumahd          #+#    #+#             */
+/*   Updated: 2022/12/09 11:51:33 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "MyHeader.hpp"
+#pragma once
 
-HumanB::HumanB(const std::string name)
-{
-    Myname = name;
-}
-
-void HumanB::attack()
-{
-    std::cout << Myname << " attack with their ";
-    if (myWeapon)
-        std::cout << myWeapon->getType();
-    else
-        std::cout << " hands :}";
-    std::cout << "\n";
-}
-
-void HumanB::setWeapon(Weapon &weapon){myWeapon = &weapon;}
+# include <iostream>
+# include <string>
+# include <fstream>
+# include <istream>
--- module 01/ex04/main.cpp
+++ module 01/ex04/main.cpp
@@ -6,30 +6,45 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:42:41 by hboumahd          #+#    #+#             */
-/*   Updated: 2022/12/09 12:25:52 by hboumahd         ###   ########.fr       */
+/*   Updated: 2022/12/09 12:11:14 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "MyHeader.hpp"
 
-int main()
+std::string getText(char *filename)
 {
-    {
-    Weapon club = Weapon("crude spiked club");
-    HumanA bob("Bob", club);
-    bob.attack();
-    club.setType("some other type of club");
-    bob.attack();
-    }
-
-    {
-    Weapon club = Weapon("crude spiked club");
-    HumanB jim("Jim");
-    // jim.setWeapon(club);
-    jim.attack();
-    club.setType("some other type of club");
-    jim.attack();
-    }
+    std::string     data;
+    std::string     line;
+    std::ifstream   infile;
+    char tmp[100]; 
     
+    infile.open(filename);
+    while (!infile.eof())
+    {
+        infile >> tmp;
+        getline(infile, line);
+        data += line + "\n";
+    }
+    infile.close();
+    return (data);
+}
+
+void replaceInFile(char *filename, char *s1, char *s2)
+{
+    std::string     text;
+    
+    text = getText(filename);
+    std::cout << s1 << "\n";
+    std::cout << s2 << "\n\n\n";
+    std::cout << text << "\n";
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 4)
+        replaceInFile(av[1], av[2], av[3]);
+    else
+        std::cout << "==> Please Enter : ./replacer <filename> s1 s2\n";
     return 0;
 }
